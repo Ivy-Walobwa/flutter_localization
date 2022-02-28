1aee2b1f9b61a59f6dbefcced77e0a993419aee1
Ivy-Walobwa <ivywalobwa@gmail.com>
2022-02-28 14:57:15 +0300

    installation and setup

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,10 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const appTitle = 'appTitle';
+  static const AlbertName = 'AlbertName';
+  static const AlbertBio = 'AlbertBio';
+  static const AkhenatenName = 'AkhenatenName';
+  static const AkhenatenBio = 'AkhenatenBio';
+
+}
